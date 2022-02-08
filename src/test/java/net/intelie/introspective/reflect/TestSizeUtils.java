990a28bb4f5674550f05a150b4edae4fad3c93e3
Cesar Eduardo Barros <cesar.barros@intelie.com.br>
2022-02-08 16:30:10 -0300

    Update all test-only dependencies

--- src/test/java/net/intelie/introspective/reflect/TestSizeUtils.java
+++ src/test/java/net/intelie/introspective/reflect/TestSizeUtils.java
@@ -1,27 +1,11 @@
 package net.intelie.introspective.reflect;
 
-import org.openjdk.jol.info.GraphPathRecord;
-import org.openjdk.jol.info.GraphVisitor;
 import org.openjdk.jol.info.GraphWalker;
 import org.openjdk.jol.vm.LightVM;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 public class TestSizeUtils {
-    public static long size(Object obj, String... ignorePrefixes) {
+    public static long size(Object obj) {
         LightVM.current();
-        GraphWalker walker = new GraphWalker(obj);
-        AtomicLong total = new AtomicLong();
-        walker.addVisitor(new GraphVisitor() {
-            @Override
-            public void visit(GraphPathRecord gpr) {
-                //if (gpr.klass().equals(char[].class) || gpr.klass().equals(String.class) || gpr.klass().equals(Double.class)) return;
-//                if (gpr.depth() == 2)
-                //System.out.println(gpr.path() + " " + gpr.size());
-                total.addAndGet(gpr.size());
-            }
-        });
-        walker.walk();
-        return total.get();
+        return new GraphWalker().walk(obj).totalSize();
     }
 }
